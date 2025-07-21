c6d224bce802a80acc34fd4e8b0f091c9237eab1
serhiy23471 <sergsy.pyatachenko@gmail.com>
2025-07-21 15:36:37 +0300

    івві

--- client/skins/index.js
+++ client/skins/index.js
@@ -1,101 +1,94 @@
-console.clear()
+console.clear();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { startup } = require('./src/utils/startup')
+const { startup } = require('./src/utils/startup');
 
-const path = require('path')
-const passport = require('passport')
-const passportSteam = require('passport-steam')
-const SteamStrategy = passportSteam.Strategy
-const session = require('express-session')
+const path = require('path');
+const passport = require('passport');
+const passportSteam = require('passport-steam');
+const SteamStrategy = passportSteam.Strategy;
+const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 
-const Logger = require('./src/utils/logger')
-const config = require('./config.json')
+const Logger = require('./src/utils/logger');
+const config = require('./config.json');
 
 const app = express();
 const port = config.PORT || 27275;
 
-let returnURL = `${config.PROTOCOL}://${config.HOST}/api/auth/steam/return`
-let realm = `${config.PROTOCOL}://${config.HOST}/`
+let returnURL;
+let realm;
 
-if (config.HOST == 'localhost' || config.HOST == '127.0.0.1') {
-    returnURL = `${config.PROTOCOL}://${config.HOST}:${config.PORT}/api/auth/steam/return`
-    realm = `${config.PROTOCOL}://${config.HOST}:${config.PORT}/`
-    
-    Logger.core.trace(`'https://ninjaproject.com.ua' at config detected, *returnURL* and *realm* changed to have port in it`)
+if (config.HOST === 'localhost' || config.HOST === '127.0.0.1') {
+  returnURL = `${config.PROTOCOL}://${config.HOST}:${config.PORT}/api/auth/steam/return`;
+  realm = `${config.PROTOCOL}://${config.HOST}:${config.PORT}/`;
+} else {
+  returnURL = `${config.PROTOCOL}://${config.HOST}/api/auth/steam/return`;
+  realm = `${config.PROTOCOL}://${config.HOST}/`;
 }
 
-// Required to get data from user for sessions
+Logger.core.info(`Steam returnURL: ${returnURL}`);
+Logger.core.info(`Steam realm: ${realm}`);
+
 passport.serializeUser((user, done) => {
     done(null, user);
 });
 passport.deserializeUser((user, done) => {
     done(null, user);
 });
-// Initiate Strategy
+
 passport.use(new SteamStrategy({
     returnURL: returnURL,
     realm: realm,
     apiKey: config.STEAMAPIKEY,
 }, function (identifier, profile, done) {
+    console.log('[SteamStrategy] identifier:', identifier);
+    console.log('[SteamStrategy] profile:', profile);
     process.nextTick(function () {
         profile.identifier = identifier;
         return done(null, profile);
     });
-}
-));
+}));
 
 const sessionStore = new MySQLStore(config.DB);
+
 app.use(session({
     store: sessionStore,
     secret: config.SESSION_SECRET,
     saveUninitialized: true,
     resave: false,
-}))
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 const mainRouter = require('./src/routes/mainRouter.route');
 
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(express.static(path.join(__dirname, '/web/public')))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '/web/public')));
 app.use('/', mainRouter);
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/web/views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/web/views'));
 
-/* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
   res.status(statusCode).json({'message': err.message});
-  
-  return;
 });
 
 const server = app.listen(port, config.INTERNAL_HOST, () => {
-    startup()
-    Logger.core.info(`App listening at ${config.PROTOCOL}:https://ninjaproject.com.ua:${port}`)
+    startup();
+    Logger.core.info(`App listening at ${config.PROTOCOL}://${config.HOST}:${port}`);
 });
 
-const io = require("socket.io")(server);
-
+const io = require('socket.io')(server);
 const weaponSocketHandler = require('./src/listeners/weapon.listener');
 
-const onConnection = (socket) => {
-    weaponSocketHandler(io, socket);
-}
-
-io.on("connection", onConnection);
+io.on('connection', socket => weaponSocketHandler(io, socket));
 
 sessionStore.onReady()
-    .then(() => {
-        Logger.sql.info('MySQLStore ready')
-    })
+  .then(() => Logger.sql.info('MySQLStore ready'));
