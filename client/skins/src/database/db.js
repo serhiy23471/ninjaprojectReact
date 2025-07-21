148310126e53155616e4cd2e07965dd13c0e0d97
serhiy23471 <sergsy.pyatachenko@gmail.com>
2025-07-21 12:29:06 +0300

    v-0.0.8

--- client/skins/src/database/db.js
+++ client/skins/src/database/db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-const config = require('../../config.json');
+const config = require('../../../config.json');
 
 const pool = mysql.createPool(config.DB);
 
