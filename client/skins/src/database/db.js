2fa02db827aa6c0e0534e6e9366f0d7d6613bcbd
serhiy23471 <sergsy.pyatachenko@gmail.com>
2025-07-21 12:37:12 +0300

    запуск

--- client/skins/src/database/db.js
+++ client/skins/src/database/db.js
@@ -1,22 +1,23 @@
 const mysql = require('mysql2/promise');
-const config = require('../../config.json');
+const path = require('path');
+
+const config = require(path.resolve(__dirname, '../../config.json'));
 
 const pool = mysql.createPool(config.DB);
 
 async function query(sql, params) {
-    let connection = await pool.getConnection()
+    let connection = await pool.getConnection();
     try {
-        let results = await connection.execute(sql)
-        results = results[0]
+        let results = await connection.execute(sql, params);
+        results = results[0];
         return results;
     } finally {
         if (connection) {
             connection.release();
         }
-
     }
 }
 
 module.exports = {
     query
-}
+};
